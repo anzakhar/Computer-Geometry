55355d49f82301cf2c56dd96ec376db5df2d989f
azaharov <azaharov@bmstu.ru>
2024-09-08 20:42:21 +0300

    Выложены л.р. №2 для 51 и 53 групп

---
+++ Лабораторные работы/51/2/2.js
@@ -0,0 +1,55 @@
+// 02.js
+
+"use strict";
+
+//// Vertex shader program
+//const VSHADER_SOURCE =
+////'attribute vec4 a_Position;\n' + // attribute variable
+//  'void main() {\n' +
+//  '  gl_Position = vec4(0.0, 0.0, 0.0, 1.0);\n' + // Set the vertex coordinates of the point
+////'  gl_Position = a_Position;\n' +
+//  '  gl_PointSize = 10.0;\n' +                    // Set the point size
+//  '}\n';
+
+//// Fragment shader program
+//const FSHADER_SOURCE =
+//  'void main() {\n' +
+//  '  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n' + // Set the point color
+//  '}\n';
+
+function main() {
+  // Retrieve <canvas> element
+    const canvas = document.getElementById('mycanvas');
+
+  // Get the rendering context for WebGL
+  const gl = getWebGLContext(canvas);
+  if (!gl) {
+    console.log('Failed to get the rendering context for WebGL');
+    return;
+  }
+
+  // Set clear color
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+  // Clear <canvas>
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  //  // Initialize shaders
+  //if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+  //    console.log('Failed to intialize shaders.');
+  //    return;
+  //  }
+
+  ////  // Get the storage location of a_Position
+  ////const a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+  ////if (a_Position < 0) {
+  ////    console.log('Failed to get the storage location of a_Position');
+  ////    return;
+  ////}
+
+  ////  // Pass vertex position to attribute variable
+  ////gl.vertexAttrib3f(a_Position, 0.0, 0.0, 0.0);
+
+  //  // Draw a point
+  //gl.drawArrays(gl.POINTS, 0, 1);
+}
---
+++ Лабораторные работы/53/2/2.js
@@ -0,0 +1,94 @@
+// 02.js
+
+"use strict";
+
+//// function circle(ctx, x, у, radius, fillCircle) {
+////  ctx.beginPath();
+////  ctx.arc (x, у, radius, 0, Math.PI * 2, false);
+////  if (fillCircle)
+////    ctx.fill();
+////  else
+////    ctx.stroke();
+//// };
+
+//// function drawBee(ctx, x, у) {
+////   ctx.lineWidth = 2;
+////   ctx.strokeStyle = "Black";
+////   ctx.fillStyle = "Gold";
+////   circle (ctx, x, у, 8, true);
+////   circle (ctx, x, у, 8, false);
+////   circle (ctx, x - 5, у - 11, 5, false);
+////   circle (ctx, x + 5, у - 11, 5, false);
+////   circle (ctx, x - 2, у - 1, 2, false);
+////   circle (ctx, x + 2, у - 1, 2, false);
+//// };
+
+//// function update( coordinate ) {
+////   const offset = Math.random( ) * 4 - 2;
+////   coordinate += offset;
+
+////    if (coordinate > 400)
+////      coordinate = 400;
+////    if (coordinate < 0) 
+////      coordinate = 0;
+
+////    return coordinate;
+////  };
+
+////// class Ball {
+//////   constructor(ctx) {
+//////     this.x = 200;
+//////     this.y = 200;
+//////     this.xSpeed = -2;
+//////     this.ySpeed = 3;
+//////     this.ctx = ctx;
+//////   }
+//////   draw() {
+//////     circle(this.ctx, this.x, this.y, 6, true);
+//////   }
+//////   move() {
+//////     this.x += this.xSpeed;
+//////     this.y += this.ySpeed;
+//////   }
+//////   checkCollision() {
+//////     if (this.x < 0 || this.x > 400) {
+//////       this.xSpeed = -this.xSpeed;
+//////     } 
+//////     if (this.y < 0 || this.y > 400)
+//////       this.ySpeed = -this.ySpeed;
+//////     } 
+////// }
+
+function main() {
+  // Retrieve <canvas> element
+    const canvas = document.getElementById('mycanvas');
+    const ctx = canvas.getContext("2d") ;
+
+    let position = 0;
+    // let size = 0;
+    //// let x = 200;
+    //// let y = 200;
+    //////const ball = new Ball(ctx);
+
+    setInterval ( function ( ) {
+      ctx.clearRect(0, 0, 400, 400);
+      ctx.fillRect(position, 0, 20, 20);
+      // ctx.fillRect(0, 0, size, size) ;
+
+      position++;
+      if (position > 400)
+         position = 0;
+
+      // size++;
+      // if ( size > 400)
+      //   size=0;
+
+      //// drawBee(ctx, x, y) ;
+      //// x = update(x);
+      //// y = update(y);
+
+      ////// ball.draw();
+      ////// ball.move();
+      ////// ball.checkCollision();
+  }, 30 ) ;
+}
