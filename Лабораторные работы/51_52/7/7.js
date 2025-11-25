10cca1049ee3361e14f52ac0fac28a85fc531889
azaharov <azaharov@bmstu.ru>
2025-11-25 13:29:55 +0300

    Обновлена л.р.№7 для группы 51,52.

--- Лабораторные работы/51_52/7/7.js
+++ Лабораторные работы/51_52/7/7.js
@@ -225,16 +225,11 @@ function initVertexBuffers(gl, model_render) {
   // Write date into the buffer object
   gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
 
-  const a_Position = gl.getAttribLocation(gl.program, 'a_Position');
-  if (a_Position < 0) {
-    console.log('Failed to get the storage location of a_Position');
-    return -1;
-  }
   // Assign the buffer object to a_Position variable
-  gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+  gl.vertexAttribPointer(1, 2, gl.FLOAT, false, 0, 0);
 
   // Enable the assignment to a_Position variable
-  gl.enableVertexAttribArray(a_Position);
+  gl.enableVertexAttribArray(1);
 
   return n;
 }
